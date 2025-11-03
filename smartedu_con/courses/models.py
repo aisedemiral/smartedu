b891ac0e58fe418f507bc3f3947cd02ddec7e29a
aisedemiral <mumineaise@gmail.com>
2025-11-03 17:37:55 +0300

    feat: Admin kısmı düzeltildi ve query ile işlemler yaptık.

---
+++ smartedu_con/courses/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True,null=True)
+    image = models.ImageField(upload_to="courses/%Y/%m/%d/", default="courses/default_courses_image.jpg")
+    date = models.DateTimeField(auto_now=True)
+    available = models.BooleanField(default=True)
+
+def _str_(self):
+    return self.name
--- smartedu_con/smartedu/settings.py
+++ smartedu_con/smartedu/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "pages.apps.PagesConfig",
+    "courses.apps.CoursesConfig"
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,7 @@ STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# MEDIA FILES
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
