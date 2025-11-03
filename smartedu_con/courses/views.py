bb7ee0ea96f7b745cc29042219c4dbd986e4a3a5
aisedemiral <mumineaise@gmail.com>
2025-11-03 18:16:39 +0300

    feat: Courses sayfamızı dinamik bir hale getirdik.

--- smartedu_con/courses/views.py
+++ smartedu_con/courses/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from . models import Course
 
-# Create your views here.
+def course_list(request):
+    course=Course.objects.all().order_by('-date')
+
+    context = {
+        'courses': course
+    }
+
+    return render(request, 'courses.html',context)
+
+ 
